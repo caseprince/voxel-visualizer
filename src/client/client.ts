c22a89b595e2072790cb6cdde909ddfb0064c8aa
Case Prince <caseprince@gmail.com>
2023-01-03 13:53:43 -0500

    Add APNG for reference

--- src/client/client.ts
+++ src/client/client.ts
@@ -34,13 +34,13 @@ const info = document.getElementById('info') as HTMLElement;
  */
 const src2 = true;
 let spritesSrc: string | null = null;
-// _8bit versions are saved with PhotoShop for 8bit 'depth' / color indexes.
+// _ps8bit versions are saved with PhotoShop for 8bit 'depth' / color indexes.
 // UPNG and ImageMagick generate PNGs with `depth: 4`, which would require some bitwise stuff to parse.
 // PhotoShop also achieves the smallest filesize.
 // TODO: Try GIFs?
 const spritesSrc200 = 'sprite-sheets/6cm_italianPaper_pavone_sprites200_ps8bit.png';
 const spritesSrc380 = 'sprite-sheets/6cm_italianPaper_pavone_sprites380_ps8bit.png';
-// spritesSrc = 'sprite-sheets/6cm_italianPaper_pavone_200layers.png'; // Animated PNGs not easy to parse, and mysteriously seem a tad larger!
+// spritesSrc = 'sprite-sheets/6cm_italianPaper_pavone_frames200.png'; // Animated PNGs not easy to parse, and mysteriously seem a tad larger!
 spritesSrc = spritesSrc200;
 
 const volume = 1000; // TODO: 3D bounding box derived from .gcvf XML
